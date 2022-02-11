d9a1e1dda7abaac2acbc3d6f6f14e71672b4bc7e
Furkan-Ahmet-Ozdemir <furkanahmetozdemir@hotmail.com>
2022-02-11 20:29:09 +0300

    Initial commit

---
+++ smartedu_con/courses/models.py
@@ -0,0 +1,17 @@
+from distutils.command.upload import upload
+from email.mime import image
+from email.policy import default
+from statistics import mode
+from unicodedata import name
+from zoneinfo import available_timezones
+from django.db import models
+
+# Create your models here.
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=200,unique=True,verbose_name="Kurs Adı",help_text="Kurs Adını Yazınız")
+    description = models.TextField(blank=True,null=True)
+    image = models.ImageField(upload_to ="courses/%Y/%m/%d/",default="courses/default_course_image.jpg")
+    date = models.DateTimeField(auto_now=True)
+    available = models.BooleanField(default=True)
