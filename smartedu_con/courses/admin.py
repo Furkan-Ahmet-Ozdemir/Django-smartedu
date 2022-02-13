eefb693723a9dd29c0d6e908f6cb4b002d747c46
Furkan-Ahmet-Ozdemir <furkanahmetozdemir@hotmail.com>
2022-02-13 13:46:00 +0300

    Courses Page

--- smartedu_con/courses/admin.py
+++ smartedu_con/courses/admin.py
@@ -1,5 +1,12 @@
+from re import search
 from django.contrib import admin
 from . models import Course
 # Register your models here.
 
-admin.site.register(Course)
+#admin.site.register(Course)
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name','available')
+    list_filter = ('available',)
+    search_fields = ('name','description')
